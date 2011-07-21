357cf7f1633e89ea5a9bb50bab7eae2b1d4371cd
Nicholas Bergson-Shilcock <me@nicholasbs.net>
2011-07-21 10:06:32 -0400

    Custom onError and onValid handlers

--- smallform.js
+++ smallform.js
@@ -3,7 +3,9 @@
     validateOnBlur: true,
     validateOnKeyUp: false,
     errorClass: 'error', // added to inputs with errors
-    validClass: 'valid' // added to valid inputs
+    validClass: 'valid', // added to valid inputs
+    onValid: null, // function called when field is valid
+    onError: null // function called when field has error
   };
 
   // Default validators
@@ -47,9 +49,11 @@
         if (errors.length > 0) {
           $(this).addClass(options.errorClass);
           $(this).removeClass(options.validClass);
+          if (typeof options.onError === 'function') options.onError.call(this, errors);
         } else {
           $(this).addClass(options.validClass);
           $(this).removeClass(options.errorClass);
+          if (typeof options.onValid === 'function') options.onValid.call(this);
         }
       }
 
